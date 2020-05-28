d8a0956c6c78cb156efadedd5b659843352ed640
Srinivas <nandusrinivas33.cse@gmail.com>
2020-05-28 20:18:46 +0530

    show NA if no language assigned, apply width to roomname column

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <div class="section">
     <div class="container">
       <div class="columns has-text-centered">
-        <div class="column">Ready to share you code with the world?</div>
+        <div class="column">Ready to share your code with the world?</div>
         <div class="column">
           <button class="button btn-secondary" @click="createRoom">Create Room</button>
         </div>
@@ -12,10 +12,10 @@
         <table class="table">
           <tr v-for="(room, rIndex) in rooms" :key="room.id">
             <td>{{ rIndex + 1 }}</td>
-            <td>
+            <td class="room-title">
               <router-link :to="{name: 'room', params: {roomId: room.id}}">{{ room.name }}</router-link>
             </td>
-            <td>{{ room.language }}</td>
+            <td>{{ room.language ? room.language : 'NA' }}</td>
           </tr>
         </table>
       </div>
@@ -49,5 +49,10 @@ export default {
 <style lang="scss" scoped>
 td {
   text-transform: capitalize;
+}
+td.room-title {
+  width: 300px;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>

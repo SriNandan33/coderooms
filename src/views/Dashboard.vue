5605305ee569addec21801bf0a861130e46c9fa7
Srinivas <nandusrinivas33.cse@gmail.com>
2020-05-28 10:02:50 +0530

    add button to create room, list rooms as table

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <router-link
-      v-for="room in rooms"
-      :key="room.id"
-      :to="{name: 'room', params: {roomId: room.id}}"
-    >
-      {{ room.name }}
-    </router-link>
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">Ready to share you code with the world?</div>
+        <div class="column">
+          <button class="button btn-secondary" @click="createRoom">Create Room</button>
+        </div>
+      </div>
+      <div v-if="rooms.length > 0">
+        <h4 class="title is-4 has-text-white has-text-centered">Your Rooms</h4>
+        <table class="table">
+          <tr v-for="(room, rIndex) in rooms" :key="room.id">
+            <td>{{ rIndex + 1 }}</td>
+            <td>
+              <router-link :to="{name: 'room', params: {roomId: room.id}}">{{ room.name }}</router-link>
+            </td>
+            <td>{{ room.language }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,9 +33,21 @@ export default {
   async created() {
     const response = await this.$http.get('rooms')
     this.rooms = response.data.rooms
+  },
+  methods: {
+    async createRoom() {
+      const response = await this.$http.post('rooms')
+      this.$router.push({
+        name: 'room',
+        params: { roomId: response.data.room.id }
+      })
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+td {
+  text-transform: capitalize;
+}
 </style>
